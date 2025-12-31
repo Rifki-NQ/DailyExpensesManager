7bed7a3f5378c0e76282447db10d85d3db87a0b2
RIFKI-NQ <rifqinf950@gmail.com>
2026-01-01 01:41:43 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+class MainMenu:
+    def __init__(self, total_menu):
+        self.total_menu = total_menu
+        self.choosen_menu = 0
+        self.choosen_sub_menu = 0
+        
+    def show_menu(self):
+        print("1. Salary DATA")
+    
+    def show_sub_menu(self):
+        pass
+    
+    def handle_choices(self):
+        pass
+    
+    def exit(self):
+        pass
+    
+    def run(self):
+        pass
+    
+if __name__ == "__main__":
+    pass
