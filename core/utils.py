415b83ceafd0cb70e938d2f1a778949f3f35a225
RIFKI-NQ <rifqinf950@gmail.com>
2026-01-06 22:17:00 +0700

    Refactor read_csv method

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import yaml
+from core.exceptions import CSVFileNotFoundError, EmptySalaryDataError, EmptyConfigDataError
 
 class CheckInput:
     @staticmethod
@@ -17,17 +18,10 @@ class DataIO:
     def __init__(self, file_path: str):
         self.file_path = file_path
     
-    def read_csv(self) -> pd.DataFrame | None:
-        try:
-            pd.set_option("display.max_rows", None)
-            df = pd.read_csv(self.file_path)
-            return df
-        except FileNotFoundError as e:
-            print(f"failed to read {e.filename} because the file does not exist!")
-            return None
-        except pd.errors.EmptyDataError:
-            print("failed to read the data because it's empty!")
-            return None
+    def read_csv(self) -> pd.DataFrame:
+        pd.set_option("display.max_rows", None)
+        df = pd.read_csv(self.file_path)
+        return df
          
     def read_config(self):
         try:
