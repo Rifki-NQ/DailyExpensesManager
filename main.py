333477acd82e44ab188bfde36a26985809f5233e
RIFKI-NQ <rifqinf950@gmail.com>
2026-01-21 21:35:52 +0700

    refactor: fix looping and quit option bug

--- main.py
+++ main.py
@@ -43,26 +43,27 @@ class MainMenu:
                 print(e)
                 
     def input_sub_menu_choices(self):
-        if self.choosen_menu == 1:
-            try:
-                self.choosen_sub_menu = input("Input by index (q to quit): ")
-                if CheckInput.check_digit(self.choosen_sub_menu, 1, self.total_sub_menu_first, quit_option=True):
-                    self.choosen_sub_menu = int(self.choosen_sub_menu)
-                    return
-                elif self.choosen_sub_menu.lower() == "q":
-                    return
-            except InvalidInputIndexError as e:
-                print(e)
-        elif self.choosen_menu == 2:
-            try:
-                self.choosen_sub_menu = input("Input by index (q to quit): ")
-                if CheckInput.check_digit(self.choosen_sub_menu, 1, self.total_sub_menu_second, quit_option=True):
-                    self.choosen_sub_menu = int(self.choosen_sub_menu)
-                    return
-                elif self.choosen_sub_menu.lower() == "q":
-                    return
-            except InvalidInputIndexError as e:
-                print(e)
+        while self.is_running:
+            if self.choosen_menu == 1:
+                try:
+                    self.choosen_sub_menu = input("Input by index (q to quit): ")
+                    if CheckInput.check_digit(self.choosen_sub_menu, 1, self.total_sub_menu_first, quit_option=True):
+                        self.choosen_sub_menu = int(self.choosen_sub_menu)
+                        break
+                    elif self.choosen_sub_menu.lower() == "q":
+                        break
+                except InvalidInputIndexError as e:
+                    print(e)
+            elif self.choosen_menu == 2:
+                try:
+                    self.choosen_sub_menu = input("Input by index (q to quit): ")
+                    if CheckInput.check_digit(self.choosen_sub_menu, 1, self.total_sub_menu_second, quit_option=True):
+                        self.choosen_sub_menu = int(self.choosen_sub_menu)
+                        break
+                    elif self.choosen_sub_menu.lower() == "q":
+                        break
+                except InvalidInputIndexError as e:
+                    print(e)
                 
     def run_choosen_method(self):
         #Salary menu
